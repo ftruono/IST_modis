50a2eaae7ecb01cf205e3e35bb6872180b5e2423
metru <metruzanca@gmail.com>
2019-08-07 12:41:14 +0200

    terapie model

---
+++ JAVA/ist/src/main/java/com/overnet/esercitazione/model/TerapieModel.java
@@ -0,0 +1,36 @@
+package com.overnet.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class TerapieModel implements Serializable {
+
+	private static final long serialVersionUID = 5772264326922388801L;
+	private Integer id;
+	private String nome;
+	private String cod;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCod() {
+		return cod;
+	}
+	public void setCod(String cod) {
+		this.cod = cod;
+	}
+
+	public TerapieModel(){}
+
+}
